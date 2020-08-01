240d45e95e3f72f4f28ccc16227c1550d45e2620
Andreas Karel Völker <andreaskvolker@gmail.com>
2020-07-31 21:42:03 -0300

    remove buid from ignore

---
+++ build/precache-manifest.388a81165780e7ec62d8bbc4b70f49d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51181a5572933c6a38b5da5ccc631aac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49f9e5021c9ee1258e65",
+    "url": "/static/css/main.64aa3297.chunk.css"
+  },
+  {
+    "revision": "c04aff44dcd11dfb34c8",
+    "url": "/static/js/2.4f5d1831.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4f5d1831.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49f9e5021c9ee1258e65",
+    "url": "/static/js/main.ea09d575.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
